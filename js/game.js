b933ace2dbbc8a64a2e6f3cf283a4335d8e95b2b
vicen <avicente@webel.com>
2022-10-10 18:44:39 +0200

    design changes

--- js/game.js
+++ js/game.js
@@ -1,13 +1,22 @@
 var ship = document.getElementById("jet");
 var board = document.getElementById("board");
+var createEnemiesInterval;
+var moveEnemiesInterval;
+var isRuning = false;
+
 const BOARD_WIDTH = 700;
+const BOARD_HEIGHT = 500;
 const SHIP_WIDTH = 60;
+const SHIP_HEIGHT = 80;
 const ENEMY_WIDTH = 65;
+const ENEMY_HEIGHT = 65;
+
 const BULLET_MOVEMENT = 3;
 const SHIP_MOVEMENT = 20;
 const ROCKET_MOVEMENT = 5;
 
 window.addEventListener("keydown", (e) => {
+  if (!isRuning) return;
   var left = parseInt(window.getComputedStyle(ship).getPropertyValue("left"));
   if (e.key == "ArrowLeft" && left > 0) {
     ship.style.left = left - SHIP_MOVEMENT + "px";
@@ -57,32 +66,43 @@ window.addEventListener("keydown", (e) => {
   }
 });
 
-var generaterocks = setInterval(() => {
-  var rock = document.createElement("div");
-  rock.classList.add("rocks");
-  //Just getting the left of the rock to place it in random position...
-  var rockleft = parseInt(window.getComputedStyle(rock).getPropertyValue("left"));
-  //generate value between 0 to 450 where 450 => board width - rock width
-  rock.style.left = Math.floor(Math.random() * (BOARD_WIDTH - ENEMY_WIDTH)) + "px";
+function playPauseGame() {
+  isRuning = !isRuning;
+  if (createEnemiesInterval == undefined && moveEnemiesInterval == undefined) createBoard();
+}
 
-  board.appendChild(rock);
-}, 1000);
+function createBoard() {
+  createEnemiesInterval = setInterval(() => {
+    if (!isRuning) return;
+    var rock = document.createElement("div");
+    rock.classList.add("rocks");
+    //Just getting the left of the rock to place it in random position...
+    var rockleft = parseInt(window.getComputedStyle(rock).getPropertyValue("left"));
+    //generate value between 0 to (board width - rock width)
+    rock.style.left = Math.floor(Math.random() * (BOARD_WIDTH - ENEMY_WIDTH)) + "px";
 
-var moverocks = setInterval(() => {
-  var rocks = document.getElementsByClassName("rocks");
-  if (rocks != undefined) {
-    for (var i = 0; i < rocks.length; i++) {
-      //Now I have to increase the top of each rock,so that the rocks can move downwards..
-      var rock = rocks[i]; //getting each rock
-      var rocktop = parseInt(window.getComputedStyle(rock).getPropertyValue("top"));
-      //475 => boardheight - rockheight + 25
-      if (rocktop >= BOARD_WIDTH - ENEMY_WIDTH + 25) {
-        alert("Game Over");
-        clearInterval(moverocks);
-        window.location.reload();
+    board.appendChild(rock);
+  }, 1000);
+
+  moveEnemiesInterval = setInterval(() => {
+    if (!isRuning) return;
+    var rocks = document.getElementsByClassName("rocks");
+    if (rocks != undefined) {
+      for (var i = 0; i < rocks.length; i++) {
+        //Now I have to increase the top of each rock,so that the rocks can move downwards..
+        var rock = rocks[i]; //getting each rock
+        var rocktop = parseInt(window.getComputedStyle(rock).getPropertyValue("top"));
+        if (rocktop >= BOARD_HEIGHT - ENEMY_HEIGHT + 25) {
+          alert("Game Over");
+          clearInterval(moveEnemiesInterval);
+          window.location.reload();
+          createEnemiesInterval = undefined;
+          moveEnemiesInterval = undefined;
+          isRuning = false;
+        }
+
+        rock.style.top = rocktop + ROCKET_MOVEMENT + "px";
       }
-
-      rock.style.top = rocktop + ROCKET_MOVEMENT + "px";
     }
-  }
-}, 100);
+  }, 100);
+}
